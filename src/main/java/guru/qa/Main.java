c7a34d89cf713e42276f66476acfd88e0b353cf3
AnnaRkchv <rakchaevaa@gmail.com>
2022-09-21 12:22:58 +0300

    First Java lesson

---
+++ src/main/java/guru/qa/Main.java
@@ -0,0 +1,40 @@
+package guru.qa;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Математические и логические операторы
+        System.out.println(1 > 2 && 3 <= 3); // false
+        System.out.println(1 > 2 || 3 <= 3); // true
+        System.out.println((2 * 5 == 20 / 2) && (7 % 2 == 3 / 2)); // true
+        System.out.println((1 + 1 == 5 - 3) || (2 - 1 == 1 + 0)); // true
+
+/*
+Переполнения при вычислениях
+
+        byte moreThanMaxByte = 128;
+        byte firstPositiveByte = 127;
+        byte secondPositiveByte = 1;
+        byte byteSum = firstPositiveByte + secondPositiveByte;
+        System.out.println(byteSum);
+
+        int lessThanMinInt = -2147483649;
+        int firstNegativeInt = -2147483647;
+        int secondNegativeInt = -2147483648;
+        System.out.println(firstNegativeInt - 1); // -> -2147483648
+        System.out.println(secondNegativeInt - 1); // -> 2147483647
+*/
+
+//        Комбинации типов данных в вычислениях
+        int firstNumber = 1;
+        double secondNumber = 1.5;
+//        int sumInt = firstNumber + secondNumber;
+//        int diffInt = firstNumber - secondNumber;
+        double sumDouble = firstNumber + secondNumber;
+        double diffDouble = firstNumber - secondNumber;
+        System.out.println(sumDouble);
+        System.out.println(diffDouble);
+
+    }
+}
